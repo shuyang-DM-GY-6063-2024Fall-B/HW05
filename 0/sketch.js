478b013b34440cc4c0b42045ccbe40c935635c93
TreeAspen <sy4254@nyu.edu>
2024-10-11 11:53:46 -0400

    Update sketch.js

--- 0/sketch.js
+++ 0/sketch.js
@@ -1,12 +1,27 @@
+let backgroundC = 'white';
+let moonX = 250;
+let moonY = 300;
+let moonR = 100;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2.5, height / 4, width / 20, height / 2);
-  rect(width - width / 2.5 - width / 20, height / 4, width / 20, height / 2);
-  rect(width / 2.5 + width / 20, height / 4 - width / 20, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
-  rect(width / 2.5 + width / 20, height / 4 + height / 2, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
+  background(backgroundC);
+
+  if (dist(mouseX, mouseY, moonX, moonY) <= moonR / 2) {
+    moonX = random(100, width - 100);
+    moonY = random(100, height - 100);
+    moonR = max(moonR - 2, 10);
+  }
+
+
+  fill('yellow');
+  ellipse(moonX, moonY, moonR);
 }
+
+
+
+ 
+
